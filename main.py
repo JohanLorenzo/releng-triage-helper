96d15eaef0327b99e2fd40fedf390a8bc952cd18
Johan Lorenzo <jlorenzo@mozilla.com>
2023-05-24 18:13:48 +0200

    Don't provide stats per current triage owner anymore

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ _PRODUCT_COMPONENTS_TO_MONITOR = (
 
 def main():
     bugs = _fetch_bugs()
-    triage_owners_per_component = _fetch_triage_owners()
 
     bug_ids_per_component = defaultdict(list)
     for bug in bugs:
@@ -26,11 +25,6 @@ def main():
     }
     print(number_of_bugs_per_component)
 
-    number_of_bugs_per_owner = defaultdict(int)
-    for component in number_of_bugs_per_component:
-        owner = triage_owners_per_component[component]
-        number_of_bugs_per_owner[owner] += number_of_bugs_per_component[component]
-    print(number_of_bugs_per_owner)
 
 
 def _fetch_bugs():
@@ -48,19 +42,6 @@ def _fetch_bugs():
         bugs.extend(req.json()["bugs"])
 
     return bugs
-
-
-def _fetch_triage_owners():
-    triage_owners_per_component = {}
-    for product_component in _PRODUCT_COMPONENTS_TO_MONITOR:
-        req = requests.get(f"{_BMO_REST_ENDPOINT}/bmo/triage_owners", params=product_component)
-        req.raise_for_status()
-        resp = req.json()
-        for product in resp.keys():
-            for component in resp[product].keys():
-                triage_owners_per_component[component] = resp[product][component]["triage_owner"]
-
-    return triage_owners_per_component
 
 
 __name__ == "__main__" and main()
